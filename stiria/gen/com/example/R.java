334f0c9daeae49ffc728f24c677ff84e45f3a981
Yuan Heng Fan <fanyuanheng@gmail.com>
2012-03-22 22:57:33 +1100

    Phonegap for andriod

--- stiria/gen/com/example/R.java
+++ stiria/gen/com/example/R.java
@@ -14,6 +14,10 @@ public final class R {
         public static final int main=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+    }
+    public static final class xml {
+        public static final int cordova=0x7f030000;
+        public static final int plugins=0x7f030001;
     }
 }
--- stiria/src/com/example/MyActivity.java
+++ stiria/src/com/example/MyActivity.java
@@ -1,15 +1,15 @@
 package com.example;
 
-import android.app.Activity;
 import android.os.Bundle;
+import org.apache.cordova.DroidGap;
 
-public class MyActivity extends Activity
+public class MyActivity extends DroidGap
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
